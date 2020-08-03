b74abe12b9641b4045b891ba2ebee190d772abe6
Rohan Peri <perirohan@gmail.com>
2020-08-03 16:27:03 -0700

    Fixing frontend

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -6,7 +6,9 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 			<p className='f3'>
 				{'This Smart Brain will detect the faces in your picture! Give it a try.'}
 			</p>
-
+			<p className='f3'>
+				{'Paste the image url of your picture! You can find this in the search bar when you choose "Open image in new tab!"'}
+			</p>
 			<div className='center'>
 				<div className='form  center pa4 br3 shadow-5'>
 					<input className='center f4 pa2 w-70 center' type='text' onChange={onInputChange}></input>
